37ba35d2cd5e91a18401d699f1d6aba8475d8c5d
Yousuf <108928113+Yousuf105654@users.noreply.github.com>
2026-09-13 21:16:53 +1000

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { register } = require('../controllers/authController');
+const { register, login, logout } = require('../controllers/authController');
 
 router.post('/register', register);
+router.post('/login', login);
+router.post('/logout', logout);
 
 module.exports = router;
-
-const { register, login } = require('../controllers/authController');
-router.post('/login', login);
-
-const { register, login, logout } = require('../controllers/authController');
-router.post('/logout', logout);
